398484eaf0f3a393faf53a2ce0fc8660e9c1d2ce
Alex Truong <87406255+alextruonglt@users.noreply.github.com>
2022-12-09 17:11:46 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='hero'>
+        <div className="temp">
+          <h1>Current Temperature</h1>
+          <div className='actual_temp'>
+            <div className="circle">
+              <p>83</p>
+            </div>
+          </div>
+        </div>
+        <div className="fan">
+          <h2>Increase Temperature</h2>
+          <div className="btn_container">
+            <button>-</button>
+            <button>+</button>
+          </div>
+        </div>
+        <div className="fan">
+          <h2>Increase Fan</h2>
+          <div className="btn_container">
+            <button>-</button>
+            <button>+</button>
+          </div>
+        </div>
+        
     </div>
-  );
+  )
 }
 
 export default App;
